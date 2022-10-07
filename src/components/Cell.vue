c13e5767a009f93fbe6dbe30658f59018c6a99d9
blanderbit <yarik.roterdamskiy@gmail.com>
2022-10-07 18:35:00 +0300

    print unescaped html entity if column value has empty string

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -9,15 +9,28 @@ export default {
     ],
 
     render (createElement) {
-        return createElement('td', {
-            class: this.cellClasses,
-            style: this.style,
-        }, [
-            createElement('span', {
-                class: this.titleClasses,
-                on: this.clickEvents,
-            }, this.value(createElement)),
-        ]);
+        const spanContentValue = this.value(createElement);
+        const hasSpanEmptyContent = spanContentValue.length === 1 && spanContentValue.includes('');
+        const spanDomProps =  {};
+
+        if (hasSpanEmptyContent) {
+            spanDomProps.innerHTML = '&nbsp;';
+        }
+        
+        const spanChildren = hasSpanEmptyContent ? [] : spanContentValue;
+        return createElement(
+            'td', {
+                class: this.cellClasses,
+                style: this.style,
+            }, 
+            [
+                createElement('span', {
+                    class: this.titleClasses,
+                    on: this.clickEvents,
+                    domProps: spanDomProps,
+                }, spanChildren),
+            ]
+        );
     },
 };
 </script>
